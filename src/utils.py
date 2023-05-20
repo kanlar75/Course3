4631e98e6b7bd068f4d9b68d1e203be83a679b63
kanlar75 <kanlar75@gmail.com>
2023-05-20 12:18:45 +0300

    добавлена функция int_input()

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,18 @@
 import json
 
-
 file_name = "operations.json"
 
 
+def int_input(text):
+    """
+    Проверяет введено ли пользователем число, если нет, то в цикле просим
+    ввести число.
+    """
+
+    while True:
+        try:
+            num = int(input(text))
+        except ValueError:
+            print("Нужно ввести ЧИСЛО операций")
+        else:
+            return num
